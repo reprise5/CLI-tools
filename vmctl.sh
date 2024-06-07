4a861cf0c404a7da37d3d4a2073af7ff75065a7c
Reprise5 <rzqxx5gv@gmail.com>
2024-06-06 20:55:09 -0400

    Add control functions to loop (clunky), try moving func STDOUTs to prgbox in TUI

--- vmctl.sh
+++ vmctl.sh
@@ -48,47 +48,91 @@ if [ -z "$action" ]; then
 	show_home
 fi
 
-#-- Based on option/action, define command to use to fill the VM option list.
-if [[ $action == "POWER ON" || $action == "SNAPSHOT" ]]; then
-	cmd="vboxmanage list vms"
-elif [[ $action == "POWER OFF" ]]; then
-	cmd="vboxmanage list runningvms"
-	#-- Check if there's any VMs running.  quit if nothing to do.
-	if [[ ! $($cmd) ]]; then
-		echo "There are no VMs running."
-		exit 0;
+while [ 1 ]; do
+	#-- Based on option/action, define command to use to fill the VM option list.
+	if [[ $action == "POWER ON" || $action == "SNAPSHOT" ]]; then
+		cmd="vboxmanage list vms"
+	elif [[ $action == "POWER OFF" ]]; then
+		cmd="vboxmanage list runningvms"
+		#-- Check if there's any VMs running.  quit if nothing to do.
+		if [[ ! $($cmd) ]]; then
+			echo "There are no VMs running."
+			exit 0;
+		fi
+		
+	fi 
+
+	options=()
+
+	#-- Get list of registered VMs to populate the checklist.
+
+	#-- State REGEX: Match the pattern "state:" and any amount of whitespace. Make a capture group and take any text in there, but stop
+	#-- when matching a pattern of any whitespace and the pattern "(since".  Within sed, we use "1" to indicate that we want to print the
+	#-- text in capture group 1.
+	while read -r vm; do
+		#vmname=$(echo "$vm" | awk -F'"' '{print $2}')
+		vmname=$(echo "$vm" | sed -r 's/^"(.*)".*$/\1/')
+		vmUUID="$(echo "$vm" | awk -F'[{}]' '{print $2}')"
+		vmstate=$(vboxmanage showvminfo $vmname | grep "State:" | sed 's/State:[[:space:]]*\(.*\)[[:space:]].*(since.*/\1/')
+		options+=($vmname "$vmstate" off)
+		((vmcount++))
+	done <<< $($cmd | sort)
+
+	#User decides on which machines they want to control.
+	if [[ $action == "POWER"* ]]; then
+		choices=`dialog --keep-tite --extra-button --extra-label "Back" --stdout --checklist "Select VMs to $action" $(($vmcount+8)) 50 $vmcount "${options[@]}"`
+	#	reset
+	elif [[ $action == "SNAPSHOT" ]]; then
+		choices=`dialog --keep-tite --extra-button --extra-label "Back" --stdout --radiolist "Select VMs to $action" $(($vmcount+8)) 50 $vmcount "${options[@]}"`
 	fi
-	
-fi 
 
-options=()
+	#-- exit code 3                    user hit back button
+	#-- exit code 0 and blank  choice: User cancelled.
+	#-- exit code 0 and filled choice: user wants to continue, so move on (break for right now)
+	if [ $? -eq 3 ]; then
+		show_home
+		choices=""
+	elif [ $choices ]; then
+	#	break;
+		if [[ $action == "POWER ON" ]]; then
+			vboxmanage startvm $choices --type headless
+			sleep 3; show_home
+		elif [[ $action == "POWER OFF" ]]; then
+			#-- Give them time to power down, or you'll freeze VirtualBox.
+			for i in $choices; do
+				#echo \"powering down $i\" && VBoxManage controlvm $i acpipowerbutton && sleep 4 | dialog --keep-tite --programbox -1 100
+				echo "powering down $i"
+				VBoxManage controlvm $i acpipowerbutton
+				sleep 4
+			done
+			sleep 3; show_home
+		elif [[ $action == "SNAPSHOT" ]]; then
+				#-- Name of the snapshot
+				snapname=`dialog --keep-tite --stdout --title "Name of the Snapshot" --inputbox "Name: " 0 50 "snap-$(date +'%m-%d-%Y_%H:%M:%S')"`
+				if [ -z $snapname ]; then
+					#snapname="snap-$(date +'%m-%d-%Y_%H:%M:%S')"
+					#-- Consider that the user cancelled the operation.  quit instead.
+	#				reset
+					exit 0
+				fi
 
-#-- Get list of registered VMs to populate the checklist.
-
-#-- State REGEX: Match the pattern "state:" and any amount of whitespace. Make a capture group and take any text in there, but stop
-#-- when matching a pattern of any whitespace and the pattern "(since".  Within sed, we use "1" to indicate that we want to print the
-#-- text in capture group 1.
-while read -r vm; do
-	#vmname=$(echo "$vm" | awk -F'"' '{print $2}')
-	vmname=$(echo "$vm" | sed -r 's/^"(.*)".*$/\1/')
-	vmUUID="$(echo "$vm" | awk -F'[{}]' '{print $2}')"
-	vmstate=$(vboxmanage showvminfo $vmname | grep "State:" | sed 's/State:[[:space:]]*\(.*\)[[:space:]].*(since.*/\1/')
-	options+=($vmname "$vmstate" off)
-	((vmcount++))
-done <<< $($cmd | sort)
-
-#User decides on which machines they want to control.
-if [[ $action == "POWER"* ]]; then
-	choices=`dialog --keep-tite --extra-button --extra-label "Back" --stdout --checklist "Select VMs to $action" $(($vmcount+8)) 50 $vmcount "${options[@]}"`
-#	reset
-elif [[ $action == "SNAPSHOT" ]]; then
-	choices=`dialog --keep-tite --extra-button --extra-label "Back" --stdout --radiolist "Select VMs to $action" $(($vmcount+8)) 50 $vmcount "${options[@]}"`
-fi
-
-if [ $? -eq 3 ]; then
-	show_home
-fi
-
+				#-- Take a running snapshot?
+				vmstate=$(vboxmanage showvminfo $choices | grep "State:" | sed 's/State:[[:space:]]*\(.*\)[[:space:]].*(since.*/\1/')
+				if [[ $vmstate == "running" ]]; then
+						live="--live"
+				fi
+	#		reset
+			#echo "Taking $([[ $vmstate == "running" ]] && echo "online" || echo "offline") "snapshot of $choices" with name \""$snapname"\""
+			#VBoxManage snapshot "$choices" take \""$snapname"\" $live
+			dialog --keep-tite --prgbox "Taking $([[ $vmstate == "running" ]] && echo "online" || echo "offline") snapshot of $choices with name \"$snapname\"" "VBoxManage snapshot "$choices" take \""$snapname"\" $live 2>&1"  30 100
+			show_home
+		fi
+	else
+		#-- Cancel was probably pressed.
+		break;
+	fi
+done
+exit 0
 #------------DEBUGGING--------------
 #reset
 #echo "the choice count here was ${#options[@]}"; exit #--DEBUG
@@ -106,41 +150,6 @@ fi
 #exit
 #------------END-DEBUGING------------
 
-#-- If user did not cancel, control the VMs.
-if [[ ! -z $choices ]]; then
-	if [[ $action == "POWER ON" ]]; then
-		vboxmanage startvm $choices --type headless
-	elif [[ $action == "POWER OFF" ]]; then
-		#-- Give them time to power down, or you'll freeze VirtualBox.
-		for i in $choices; do
-			echo "powering down $i"
-			VBoxManage controlvm $i acpipowerbutton
-			sleep 4
-		done
-	elif [[ $action == "SNAPSHOT" ]]; then
-			#-- Name of the snapshot
-			snapname=`dialog --keep-tite --stdout --title "Name of the Snapshot" --inputbox "Name: " 0 50 "snap-$(date +'%m-%d-%Y_%H:%M:%S')"`
-			if [ -z $snapname ]; then
-				#snapname="snap-$(date +'%m-%d-%Y_%H:%M:%S')"
-				#-- Consider that the user cancelled the operation.  quit instead.
-#				reset
-				exit 0
-			fi
-
-			#-- Take a running snapshot?
-			vmstate=$(vboxmanage showvminfo $choices | grep "State:" | sed 's/State:[[:space:]]*\(.*\)[[:space:]].*(since.*/\1/')
-			if [[ $vmstate == "running" ]]; then
-					live="--live"
-			fi
-#		reset
-		echo "Taking $([[ $vmstate == "running" ]] && echo "online" || echo "offline") "snapshot of $choices" with name \""$snapname"\""
-		VBoxManage snapshot "$choices" take \""$snapname"\" $live
-	fi
-else
-	#-- If user cancelled, clean their terminal.
-#	reset
-	exit
-fi
 #-- TODO:
 #--   handle VM in states "paused" and "saved".  
 #--   Saved can be started or discarded, paused cannot be discarded, and must be unpaused.
